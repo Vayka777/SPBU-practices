3ca84815c3f20455d0099ab83ff5aa407fb554e1
Vi <filatova_002@mail.ru>
2021-10-07 23:38:12 +0300

    The first task of practice (easy)

---
+++ 15.10.2021_HW_medium/15.10.2021_HW_medium/15.10.2021_HW_medium.cpp
@@ -0,0 +1,51 @@
+﻿/*Создайте динамический двумерный массив и заполните его
+случайными  числами от 10 до 50. Вывести его в консоль.
+Для напоплнения и визуализации необходимо написать отдельные функции*/
+
+#include <iostream>
+#include <ctime>
+
+void input(int** a, int M, int N)
+{
+    int i, j;
+    srand(time(NULL));
+    for (i = 0; i < M; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            a[i][j] = rand() % 50 + 10;
+        }
+    }
+}
+
+void print(int** a, int M, int N)
+{
+    int i, j;
+    for (i = 0; i < M; i++)
+    {
+        for (j = 0; j < N; j++)
+        {
+            std::cout << a[i][j] << " ";
+        }
+        std::cout << "\n";
+    }
+}
+
+int main()
+{
+    int i, M, N;
+    std::cout << "Input M: ";
+    std::cin >> M;
+    std::cout << "Imput N: ";
+    std::cin >> N;
+    int** arr = new int* [M];
+    for (i = 0; i < M; i++)
+    {
+        arr[i] = new int[N];
+    }
+    input(arr, M, N);
+    std::cout << "\nArray: \n";
+    print(arr, M, N);
+
+    return 0;
+}
