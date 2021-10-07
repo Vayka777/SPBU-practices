affa926a419f4e877c98ddadfbf0f1a117408e64
Vi <filatova_002@mail.ru>
2021-10-07 23:38:12 +0300

    The first task of practice (easy)

---
+++ ea/ea/ea.cpp
@@ -0,0 +1,27 @@
+﻿// ea
+#include <iostream>
+
+void main()
+{
+    int i, N;
+    std::cout << "Input N: ";
+    std::cin >> N;
+    if ((1 <= N) && (N <= 15))
+    {
+        for (i = 0; i < N; i++)
+        {
+            int x1, x2, y1, y2;
+            std::cout << "\nInput coordinates of point a: \n" << "Input x1: ";
+            std::cin >> x1;
+            std::cout << "Input y1: ";
+            std::cin >> y1;
+            std::cout << "Input coordinates of point b: \n" << "Input x2: ";
+            std::cin >> x2;
+            std::cout << "Input y2: ";
+            std::cin >> y2;
+            std::cout << "Coordinates of point c: \n" << "x3: " << (y1 - y2) + x2 << std::endl;
+            std::cout << "y3: " << -(x1 - x2) + y2 << "\n" << std::endl;
+        }
+    }
+    else std::cout << "ERROR\n" << std::endl;
+}
